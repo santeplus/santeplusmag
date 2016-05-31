965b7adabedb389d786f886a1fa40e9a3fc7f57b
ysfbsf <youssef.bsf@gmail.com>
2016-05-31 13:00:16 +0100

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name santeplusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the santeplusApp
+ */
+angular.module('santeplusApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
