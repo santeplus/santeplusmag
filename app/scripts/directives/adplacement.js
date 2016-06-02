b11d7e2e76d6a6b7d8bcddcb82a06d9665ca6d07
ysfbsf <youssef.bsf@gmail.com>
2016-06-02 11:00:02 +0100

    Page Article

---
+++ app/scripts/directives/adplacement.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name santeplusApp.directive:adPlacement
+ * @description
+ * # adPlacement
+ */
+angular.module('santeplusApp')
+  .directive('adPlacement', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      replace: true,
+      scope : {
+      	  containerId: '@',
+          adClient : '@',
+          adSlot : '@',
+          inlineStyle : '@',
+          adFormat : '@'
+      },
+      link: function postLink(scope, element, attrs) {
+      	console.log($("#" + containerId).html());
+      }
+    };
+  });
